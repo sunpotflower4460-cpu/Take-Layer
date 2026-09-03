add1e911bd3a5d776ad090550c18ce4587bbcac2
sunpotflower4460-cpu <sunpotflower4460@gmail.com>
2026-09-03 16:19:50 +0900

    Remove misleading camera audio export setting

--- TakeLayer/Models/ExportSettings.swift
+++ TakeLayer/Models/ExportSettings.swift
@@ -1,22 +1,21 @@
 import Foundation
 
 struct ExportSettings: Codable {
-    var muteCameraAudio: Bool = true
     var outputResolution: OutputResolution = .hd1080p
     var outputFileType: OutputFileType = .mp4
 }
 
-enum OutputResolution: String, CaseIterable, Identifiable, Codable {
-    case hd1080p = "1080p"
-
-    var id: String { rawValue }
-    var maxLongEdge: Double { 1920 }
-    var maxShortEdge: Double { 1080 }
+enum OutputResolution: String, Codable, CaseIterable {
+    case hd1080p = "1920x1080"
 }
 
-enum OutputFileType: String, CaseIterable, Identifiable, Codable {
+enum OutputFileType: String, Codable, CaseIterable {
     case mp4 = "MP4"
 
-    var id: String { rawValue }
-    var fileExtension: String { "mp4" }
+    var fileExtension: String {
+        switch self {
+        case .mp4:
+            return "mp4"
+        }
+    }
 }
